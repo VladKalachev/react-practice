b546a58339ef0b78307be2027b613fee7f520a21
Corvax <vlvete1990@yandex.ru>
2018-05-13 13:16:41 +0300

    add react-select

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,35 @@
 import React, { Component } from 'react'
 import ArticleList from './ArticleList'
 import UserForm from './UserForm'
-
+import Select from 'react-select'
+import 'react-select/dist/react-select.css';
+import {articles} from "../fixtures";
 
 class App extends Component {
 
 
     state = {
-        username: ''
+        selections: null
     }
 
     render(){
+
+        const options = this.props.articles.map(item => ({
+            value: item.id, label: item.title,
+        }))
+
+        console.log(options)
+
+        console.log(this.state)
         return <div>
             <UserForm />
+            <Select options={options} value={this.state.selections} onChange={this.chengeSelection} multi={true}/>
+
             <ArticleList {...this.props}/>
         </div>
     }
 
+    chengeSelection = selections => this.setState({ selections })
 
 }
 
